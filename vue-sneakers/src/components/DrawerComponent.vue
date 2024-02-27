cad371f7a37398c81d26a0b031aa4e8070524edd
Kaisar Yerdenbekov <sysreqx@gmail.com>
2024-02-27 14:13:20 +0600

    feature(all): cart

--- vue-sneakers/src/components/DrawerComponent.vue
+++ vue-sneakers/src/components/DrawerComponent.vue
@@ -1,18 +1,36 @@
 <script setup>
 
 import DrawerHead from "@/components/DrawerHead.vue";
-import CartItem from "@/components/CartItem.vue";
+import CartItemList from "@/components/CartItemList.vue";
 </script>
 
 <template>
   <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70">
   </div>
-  <div class=" bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
+
+  <div class=" bg-white w-96 h-full fixed right-0 top-0 z-20 p-8 mb-6">
     <drawer-head />
-    <cart-item />
-    <cart-item />
-    <cart-item />
-    <cart-item />
+    <cart-item-list />
+
+    <div class="flex flex-col gap-4 mt-7">
+      <div class="flex gap-2">
+        <span>Итого:</span>
+        <div class="flex-1 border-b border-dashed"></div>
+        <b>12990 Р</b>
+      </div>
+
+      <div class="flex gap-2">
+        <span>Налог 5%:</span>
+        <div class="flex-1 border-b border-dashed"></div>
+        <b>12990 Р</b>
+      </div>
+
+      <button
+          disabled=""
+          class="bg-lime-500 w-full rounded-xl py-3 text-white disabled:bg-slate-400 hover:bg-lime-600 transition active:bg-lime-700 cursor-pointer mt-4">Оформить заказ</button>
+    </div>
+
+
   </div>
 </template>
 
